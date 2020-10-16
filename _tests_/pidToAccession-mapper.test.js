5c0c73dc513f2fc25a381dd02720134dbd563972
vibinernesto <vibinernesto@gmail.com>
2020-10-16 18:33:18 +0530

    Pushing the changses with structured folder for pushing the JS files

--- _tests_/pidToAccession-mapper.test.js
+++ _tests_/pidToAccession-mapper.test.js
@@ -1,12 +1,6 @@
 // test data for mirth code funtionality 
 // purpose -- mapping OBr.2.1 to PID.2.1
-//import mapPidToAccessionNumber from '../MirthCode/mapPidToAccessionNumber.js';
-
-function mapPidToAccessionNumber(incomingFile,pid){
-  var msg = JSON.parse(incomingFile);
-  msg.HL7Message.OBR["OBR.2"] = {"OBR.2.1":msg.HL7Message.PID["PID.2"]["PID.2.1"].toString()};
-  return JSON.stringify(msg);
-}
+const mapPidToAccessionNumber = require('../MirthCode/mapPidToAccessionNumber.js');
 
 describe("Maping Function",()=>{
     test("pid 2 should be mapped to obr.2",()=>{
